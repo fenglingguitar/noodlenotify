aac405b7c5224cffbdd82bc9788718db2074ccbc
fenglingguitar <fenglingguitar@163.com>
2016-10-13 17:24:55 +0800

    BodyCacheCapacity状态监测第一次优化

--- noodlenotify-monitor-status/src/main/java/org/fl/noodlenotify/monitor/status/executer/service/impl/MsgBodyCacheCapacityStatusExecuterServiceImpl.java
+++ noodlenotify-monitor-status/src/main/java/org/fl/noodlenotify/monitor/status/executer/service/impl/MsgBodyCacheCapacityStatusExecuterServiceImpl.java
@@ -2,19 +2,20 @@ package org.fl.noodlenotify.monitor.status.executer.service.impl;
 
 import java.util.List;
 
+import org.fl.noodlenotify.console.constant.ConsoleConstants;
+import org.fl.noodlenotify.console.service.MsgBodyCacheService;
+import org.fl.noodlenotify.console.vo.MsgBodyCacheVo;
+import org.fl.noodlenotify.core.connect.ConnectAgent;
+import org.fl.noodlenotify.core.connect.ConnectAgentFactory;
+import org.fl.noodlenotify.core.connect.cache.body.BodyCacheStatusChecker;
+import org.fl.noodlenotify.monitor.status.executer.service.ExecuterServiceAbstract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.fl.noodlenotify.console.constant.ConsoleConstants;
-import org.fl.noodlenotify.console.service.MsgBodyCacheService;
-import org.fl.noodlenotify.console.vo.MsgBodyCacheVo;
-import org.fl.noodlenotify.core.connect.cache.body.BodyCacheStatusChecker;
-import org.fl.noodlenotify.core.connect.cache.body.manager.console.ConsoleBodyCacheConnectManager;
-import org.fl.noodlenotify.monitor.status.executer.service.ExecuterServiceAbstract;
 
 
-@Service("msgBodyCacheCapacityStatusExecuterService")
+@Service("msgBodyCacheCapacityStatusExecuter")
 public class MsgBodyCacheCapacityStatusExecuterServiceImpl extends ExecuterServiceAbstract {
 
 	private final static Logger logger = LoggerFactory.getLogger(MsgBodyCacheCapacityStatusExecuterServiceImpl.class);
@@ -23,28 +24,29 @@ public class MsgBodyCacheCapacityStatusExecuterServiceImpl extends ExecuterServi
 	private MsgBodyCacheService msgBodyCacheService;
 
 	@Autowired
-	ConsoleBodyCacheConnectManager consoleBodyCacheConnectManager;
+	private ConnectAgentFactory redisBodyCacheConnectAgentFactory;
 
 	@Override
 	public void execute() throws Exception {
 		
 		MsgBodyCacheVo msgBodyCacheVoParam = new MsgBodyCacheVo();
 		msgBodyCacheVoParam.setManual_Status(ConsoleConstants.MANUAL_STATUS_VALID);
-		List<MsgBodyCacheVo> msgBodyCaches = msgBodyCacheService.queryMsgBodyCacheList(msgBodyCacheVoParam);
-		for (MsgBodyCacheVo msgBodyCache : msgBodyCaches) {
-			BodyCacheStatusChecker queueCacheStatusChecker = (BodyCacheStatusChecker) consoleBodyCacheConnectManager.getConnectAgent(msgBodyCache.getMsgBodyCache_Id());
-			if (queueCacheStatusChecker != null) {
-				try {
-					long size = queueCacheStatusChecker.checkSize();
-					MsgBodyCacheVo currentmsgBodyCacheVo = new MsgBodyCacheVo();
-					currentmsgBodyCacheVo.setMsgBodyCache_Id(msgBodyCache.getMsgBodyCache_Id());
-					currentmsgBodyCacheVo.setSize(size);
-					msgBodyCacheService.updatesMsgBodyCacheSize(currentmsgBodyCacheVo);
-				} catch (Exception e) {
-					if (logger.isDebugEnabled()) {
-						logger.error("CheckSize -> " + e);
-					}
+		List<MsgBodyCacheVo> msgBodyCacheVoList = msgBodyCacheService.queryMsgBodyCacheList(msgBodyCacheVoParam);
+		for (MsgBodyCacheVo msgBodyCacheVo : msgBodyCacheVoList) {
+			ConnectAgent connectAgent = redisBodyCacheConnectAgentFactory.createConnectAgent(msgBodyCacheVo.getIp(), msgBodyCacheVo.getPort(), msgBodyCacheVo.getMsgBodyCache_Id());
+			try {
+				connectAgent.connect();
+				long size = ((BodyCacheStatusChecker)connectAgent).checkSize();
+				MsgBodyCacheVo currentmsgBodyCacheVo = new MsgBodyCacheVo();
+				currentmsgBodyCacheVo.setMsgBodyCache_Id(msgBodyCacheVo.getMsgBodyCache_Id());
+				currentmsgBodyCacheVo.setSize(size);
+				msgBodyCacheService.updatesMsgBodyCacheSize(currentmsgBodyCacheVo);
+			} catch (Exception e) {
+				if (logger.isDebugEnabled()) {
+					logger.error("CheckSize -> " + e);
 				}
+			} finally {
+				connectAgent.close();
 			}
 		}
 	}
