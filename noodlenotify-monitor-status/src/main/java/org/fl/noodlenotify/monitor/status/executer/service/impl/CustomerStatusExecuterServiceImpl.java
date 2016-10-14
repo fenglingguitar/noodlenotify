cde165db4bfca86663552ebb21a1a0793dd26371
fenglingguitar <fenglingguitar@163.com>
2016-10-14 15:01:47 +0800

    Customer状态监测第一次优化

--- noodlenotify-monitor-status/src/main/java/org/fl/noodlenotify/monitor/status/executer/service/impl/CustomerStatusExecuterServiceImpl.java
+++ noodlenotify-monitor-status/src/main/java/org/fl/noodlenotify/monitor/status/executer/service/impl/CustomerStatusExecuterServiceImpl.java
@@ -1,17 +1,19 @@
 package org.fl.noodlenotify.monitor.status.executer.service.impl;
 
 import java.util.List;
+import java.util.Map;
 
+import org.fl.noodlenotify.console.constant.ConsoleConstants;
+import org.fl.noodlenotify.console.service.CustomerService;
+import org.fl.noodlenotify.console.vo.CustomerVo;
+import org.fl.noodlenotify.core.connect.ConnectAgent;
+import org.fl.noodlenotify.core.connect.ConnectAgentFactory;
+import org.fl.noodlenotify.core.connect.net.NetStatusChecker;
+import org.fl.noodlenotify.monitor.status.executer.service.ExecuterServiceAbstract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.fl.noodlenotify.console.constant.ConsoleConstants;
-import org.fl.noodlenotify.console.service.CustomerService;
-import org.fl.noodlenotify.console.vo.CustomerVo;
-import org.fl.noodlenotify.core.connect.net.NetStatusChecker;
-import org.fl.noodlenotify.core.connect.net.manager.console.ConsumerConsoleNetConnectManager;
-import org.fl.noodlenotify.monitor.status.executer.service.ExecuterServiceAbstract;
 
 @Service("customerStatusExecuterService")
 public class CustomerStatusExecuterServiceImpl extends ExecuterServiceAbstract {
@@ -22,7 +24,7 @@ public class CustomerStatusExecuterServiceImpl extends ExecuterServiceAbstract {
 	private CustomerService customerService;
 
 	@Autowired
-	ConsumerConsoleNetConnectManager customerConsoleNetConnectManager;
+	private Map<String, ConnectAgentFactory> connectAgentFactoryMap;
 
 	@Override
 	public void execute() throws Exception {
@@ -33,15 +35,18 @@ public class CustomerStatusExecuterServiceImpl extends ExecuterServiceAbstract {
 		for (CustomerVo customerVo : customerVoList) {
 			byte systemStatus = customerVo.getSystem_Status();
 			byte currentSysTemStatus = ConsoleConstants.SYSTEM_STATUS_OFF_LINE;
-			NetStatusChecker netStatusChecker = (NetStatusChecker) customerConsoleNetConnectManager.getConnectAgent(customerVo.getCustomer_Id());
-			if (netStatusChecker != null) {
-				try {
-					netStatusChecker.checkHealth();
-					currentSysTemStatus = ConsoleConstants.SYSTEM_STATUS_ON_LINE;
-				} catch (Exception e) {
-					if (logger.isDebugEnabled()) {
-						logger.error("CheckHealth -> " + e);
-					}
+			ConnectAgentFactory connectAgentFactory = connectAgentFactoryMap.get(customerVo.getCheck_Type());
+			if (connectAgentFactory != null) {
+				
+			}
+			ConnectAgent connectAgent = connectAgentFactory.createConnectAgent(customerVo.getIp(), customerVo.getCheck_Port(), customerVo.getCustomer_Id());
+			try {
+				connectAgent.connect();
+				((NetStatusChecker)connectAgent).checkHealth();
+				currentSysTemStatus = ConsoleConstants.SYSTEM_STATUS_ON_LINE;
+			} catch (Exception e) {
+				if (logger.isDebugEnabled()) {
+					logger.error("CheckHealth -> " + e);
 				}
 			}
 			if (systemStatus != currentSysTemStatus) {
@@ -51,5 +56,9 @@ public class CustomerStatusExecuterServiceImpl extends ExecuterServiceAbstract {
 				customerService.updateCustomerSystemStatus(currentCustomerVo);
 			}
 		}
+	}
+
+	public void setConnectAgentFactoryMap(Map<String, ConnectAgentFactory> connectAgentFactoryMap) {
+		this.connectAgentFactoryMap = connectAgentFactoryMap;
 	}
 }
